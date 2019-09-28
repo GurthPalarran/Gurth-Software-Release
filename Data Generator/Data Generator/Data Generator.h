868f8d8f1041cbfc0bef2454a5f6bdb1b3d63a92
Seregon Jee <gurthpalarran@outlook.com>
2019-09-28 19:50:28 +0800

    Create Data Generator.h

---
+++ Data Generator/Data Generator/Data Generator.h
@@ -0,0 +1,32 @@
+
+// Data Generator.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CDataGeneratorApp:
+// See Data Generator.cpp for the implementation of this class
+//
+
+class CDataGeneratorApp : public CWinApp
+{
+public:
+	CDataGeneratorApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CDataGeneratorApp theApp;
