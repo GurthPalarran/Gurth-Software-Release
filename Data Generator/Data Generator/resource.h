faaa6e411c47a6c39a493d777e708ad18d794645
Seregon Jee <gurthpalarran@outlook.com>
2019-09-28 19:51:19 +0800

    Create resource.h

---
+++ Data Generator/Data Generator/resource.h
@@ -0,0 +1,67 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 DataGenerator.rc 使用
+//
+#define ID_GENERATE                     2
+#define ID_SETTING                      3
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_DATAGENERATOR_DIALOG        102
+#define IDR_MAINFRAME                   128
+#define IDD_SETTING                     130
+#define ID_CANCEL                       1000
+#define ID_ARGOK                        1001
+#define ID_ARGCANCEL                    1002
+#define IDC_EDIT_VISCOSiITY             1003
+#define IDC_EDIT_CORRECTION             1004
+#define IDC_EDIT_LENGTH                 1005
+#define IDC_EDIT_ATMOP                  1006
+#define IDC_EDIT_DISTANCE               1007
+#define IDC_EDIT_GRAVITY                1008
+#define IDC_EDIT_DENSITY                1009
+#define IDC_CHECK_INVERSE               1011
+#define IDC_TIME2                       1012
+#define IDC_ITERATION                   1013
+#define IDC_ITERATION_                  1014
+#define IDC_MAX_ELE                     1015
+#define IDC_MAX_ELE_                    1016
+#define IDC_MAX_V_                      1017
+#define IDC_EDIT3                       1018
+#define IDC_MAX_V                       1018
+#define IDC_VOLTAGE1                    1023
+#define IDC_CHARGE1                     1024
+#define IDC_TIME1                       1025
+#define IDC_DECIATION1                  1026
+#define IDC_ELECTRON1                   1027
+#define IDC_VOLTAGE2                    1028
+#define IDC_ELECTRON2                   1029
+#define IDC_CHARGE2                     1030
+#define IDC_DECIATION2                  1031
+#define IDC_TIME3                       1032
+#define IDC_VOLTAGE3                    1033
+#define IDC_ELECTRON3                   1034
+#define IDC_CHARGE3                     1035
+#define IDC_DECIATION3                  1036
+#define IDC_TIME4                       1037
+#define IDC_VOLTAGE4                    1038
+#define IDC_ELECTRON4                   1039
+#define IDC_CHARGE4                     1040
+#define IDC_EDIT20                      1041
+#define IDC_DECIATION4                  1041
+#define IDC_ELECHARGE1                  1042
+#define IDC_ELECHARGE2                  1043
+#define IDC_ELECHARGE3                  1044
+#define IDC_DECIATION8                  1045
+#define IDC_ELECHARGE4                  1045
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1019
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
